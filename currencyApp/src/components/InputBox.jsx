c3117292c6ece2647531fa08c5dc264f314b9cc7
Shree-crypto <60309661+Shree-crypto@users.noreply.github.com>
2024-05-25 16:36:12 +0530

    currency inputbox completed

--- currencyApp/src/components/InputBox.jsx
+++ currencyApp/src/components/InputBox.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 function InputBox({
   label,
-
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOption = [],
+  selectCurrency = "usd",
   className = "",
 }) {
   return (
@@ -13,12 +17,24 @@ function InputBox({
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
+          value={amount}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
         />
       </div>
       <div className="w-1/2 flex flex-wrap justify-end text-right">
         <p className="text-black/40 mb-2 w-full">Currency Type</p>
-        <select className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none">
-          <option value="usd">usd</option>
+        <select
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          value={selectCurrency}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+        >
+          {currencyOption.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          )}
         </select>
       </div>
     </div>
